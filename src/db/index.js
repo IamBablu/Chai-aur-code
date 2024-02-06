206cb277339580d937297804256565ad1bf73d2f
iamBablu <bablupamdey45671@gmail.com>
2024-02-06 08:56:42 +0530

    connection stablish

--- src/db/index.js
+++ src/db/index.js
@@ -4,7 +4,7 @@ import { dbName } from "../constants.js";
 const connectDB = async ()=>{
     try{
         const connectionInstants = await mongoose.connect(`${process.env.MONGODB_URL}/${dbName}`)
-        console.log(`\n mongos db connected !!! db host : ${connectionInstants.connection.host}`)
+        console.log(`\n mongos db connected !! db host : ${connectionInstants.connection.host}`)
     }catch(error){
         console.log("Error",error)
         process.exit(1)
